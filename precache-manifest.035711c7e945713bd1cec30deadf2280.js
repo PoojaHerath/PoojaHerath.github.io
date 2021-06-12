f9d182d444d8b5d06767fb3b75de9b0f62a483e1
Poojani Herath <92888@fhss.sjp.ac.lk>
2021-06-12 11:00:13 +0530

    Updates

---
+++ precache-manifest.035711c7e945713bd1cec30deadf2280.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e708212601029132bd149df07a1e61be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a32459649f481b70edef",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a32459649f481b70edef",
+    "url": "/static/js/main.54cde434.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "51cd696e614a57d45a6a2be1af1371c9",
+    "url": "/static/media/mypic.51cd696e.jpg"
+  },
+  {
+    "revision": "24960acf56dd2f390064e4b8f5adafc4",
+    "url": "/static/media/resume.24960acf.pdf"
+  }
+]);
